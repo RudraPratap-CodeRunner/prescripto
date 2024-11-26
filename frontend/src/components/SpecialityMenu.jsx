4e4ceacde7ece2f1853c34bbcfcf0554f8744ebe
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-11-26 19:01:31 +0530

    Appointment page added

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -3,7 +3,7 @@ import { specialityData } from '../assets/assets'
 import {Link} from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
-    <div id='#speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
+    <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
         <h1 className='text-3xl font-medium'>Find by Speciality</h1>
         <p className='sm:w-2/3 text-sm text-center'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
         <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
